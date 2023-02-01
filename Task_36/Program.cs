c2a549d3cb066d2603ae00246296819c59dfc20a
Osa <safronova.2104@mail.ru>
2023-02-01 18:27:31 +0300

    Решение

---
+++ Task_36/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine($" -> {GetSumOddPlace(arr)}");
+
+int GetSumOddPlace(int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i %2 == 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+
+int [] GetRandomArray(int length)
+{
+    int[] result = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random(). Next (-100, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+}
---
+++ Task_38/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементом массива.
+//[3 7 22 2 78] -> 76
+
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine($" -> {GetDiffMinMax(arr)}");
+
+int GetDiffMinMax(int[] array)
+{
+    int result = 0;
+    int _Max = arr[0], _Min = arr[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] > _Max)
+        _Max = arr[i];
+
+        if (arr[i] < _Min)
+        _Min = arr[i];
+    }
+    result = _Max - _Min;
+    return result;
+}
+
+int [] GetRandomArray(int length)
+{
+    int[] result = new int [length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random(). Next (0, 100);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+}
